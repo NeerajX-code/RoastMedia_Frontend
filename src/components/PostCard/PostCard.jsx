ee9ec5112a19a402fc6949f1047b9fd638cd7576
NeerajX-code <neerajrathore2090@gmail.com>
2025-08-25 14:29:17 +0530

    home page theme change

--- src/components/PostCard/PostCard.jsx
+++ src/components/PostCard/PostCard.jsx
@@ -25,7 +25,12 @@ const PostCard = ({ post }) => {
                             <p>@{post.userData?.username}</p>
                         </div>
                         <div className="userDetails_right">
-                            <p>{post.createdAt}</p>
+                            <p>
+                                {new Intl.DateTimeFormat("en-US", {
+                                    day: "2-digit",
+                                    month: "short",
+                                    year: "numeric",
+                                }).format(new Date(post.createdAt))}</p>
                         </div>
                     </div>
                 </div>
